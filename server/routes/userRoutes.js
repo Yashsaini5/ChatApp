ba2f0e40948890744053596219e72bf131858a59
Yash Saini <yashsaini0505@gmail.com>
2025-09-01 17:17:21 +0530

    auth change

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const router = express.Router()
-const {authMiddleware} = require("../middleware/authmiddleware")
+const {authMiddleware} = require("../middleware/authMiddleware")
 const { createUser , loginUser, getUser, getAllUser} = require('../controller/userController')
 
 router.post("/signup",createUser)
