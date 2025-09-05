50daca47f16a975670f94c3a2456ceffbbaee608
Yash Saini <yashsaini0505@gmail.com>
2025-09-05 13:26:40 +0530

    testing for socket problem

--- client/src/pages/ChatPage.jsx
+++ client/src/pages/ChatPage.jsx
@@ -24,7 +24,7 @@ const ChatPage = ({isMobile}) => {
   useEffect(() => {
     if (!user?.email) return;
 
-    socketRef.current = io(import.meta.env.VITE_BACKEND_URL, {
+    socketRef.current = io("https://chatapp-pkj7.onrender.com", {
       withCredentials: true,
       transports: ["websocket"]
     });
